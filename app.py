7e3bd262ccd89e72c85673a6b702aad94f4b3f12
Mikk0git <kasprzakmilan1@gmail.com>
2024-03-15 20:07:30 +0100

    basic flask rest api

--- app.py
+++ app.py
@@ -1,52 +1,56 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 
 def get_substitutions(schoolName, date):
     url = f'https://{schoolName}.edupage.org/substitution/server/viewer.js?__func=getSubstViewerDayDataHtml'
-    body = {"__args": [None, {"date": f"{date}", "mode": "classes"}], "__gsh": "00000000"}
+    body = {"__args": [None, {"date": f"{date}",
+                              "mode": "classes"}], "__gsh": "00000000"}
 
     res = requests.post(url, json=body)
+    if res.status_code != 200:
+        return {"error": "Error retrieving substitutions."}, 500
 
     data = json.loads(res.text)
-    soup = BeautifulSoup( data["r"],"html.parser" )
-    # print(soup.prettify())
+    soup = BeautifulSoup(data["r"], "html.parser")
 
+    finalJson = {"all_substitutions": []}
 
-    finalJson = {
-        "all_substitutions":[]
-            }
-    
     classSubstitutions = soup.find_all("div", class_="print-nobreak")
 
-
     for classData in classSubstitutions:
-
         className = classData.find("div", class_="header").string
-
-        # print(className)
-
         substitutions = classData.find_all("div", class_="row")
         substitutionsArray = []
         for substitution in substitutions:
-
             hour = substitution.find("div", class_="period").string
-            # print(hour)
-        
-            info = substitution.find("div", class_="info").string 
-            # print(info)
+            info = substitution.find("div", class_="info").string
+            substitutionsArray.append({"hour": hour, "info": info})
 
-            substitutionsArray.append({"hour":hour, "info":info})
-           
+        if className:
+            finalJson["all_substitutions"].append({
+                "name": className,
+                "substitutions": substitutionsArray
+            })
 
-        if className: 
-          finalJson["all_substitutions"].append({
-            "name": className,
-            "substitutions":substitutionsArray
-        })
+    return finalJson, 200
 
 
-    
-    return finalJson
-    
-print(get_substitutions("zs2ostrzeszow", "2024-03-15"))
+@app.route('/get_substitutions', methods=['GET'])
+def substitutions_endpoint():
+    school_name = request.args.get('school_name')
+    date = request.args.get('date')
+
+    if school_name is None or date is None:
+        return jsonify({"error": "Please provide both 'school_name' and 'date' parameters."}), 400
+
+    substitutions, status_code = get_substitutions(school_name, date)
+    return jsonify(substitutions), status_code
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # You can set debug=False for production
