77e572685179fc0a6460561d58bef8b9796dc739
Mikk0git <kasprzakmilan1@gmail.com>
2024-03-15 19:31:15 +0100

    get_substitutions returns  json with substitutions

--- app.py
+++ app.py
@@ -1,8 +1,56 @@
 import requests
+import json
+from bs4 import BeautifulSoup
 
-url = 'https://lo2gdy.edupage.org/substitution/server/viewer.js?__func=getSubstViewerDayDataHtml'
-body = {"__args": [None, {"date": "2024-03-08", "mode": "classes"}], "__gsh": "00000000"}
+def get_substitutions(schoolName, date):
+    url = f'https://{schoolName}.edupage.org/substitution/server/viewer.js?__func=getSubstViewerDayDataHtml'
+    body = {"__args": [None, {"date": f"{date}", "mode": "classes"}], "__gsh": "00000000"}
 
-r = requests.post(url, json=body)
+    res = requests.post(url, json=body)
 
-print(r.text)
+    data = json.loads(res.text)
+    soup = BeautifulSoup( data["r"],"html.parser" )
+    # print(soup.prettify())
+
+
+    finalJson = {
+        "absentTeachers":None,
+        "absentClasses":None,
+        "unavalibleClassRooms":None,
+        "substitutions":[],
+            }
+    
+    
+    classSubstitutions = soup.find_all("div", class_="print-nobreak")
+
+
+    for classData in classSubstitutions:
+
+        className = classData.find("div", class_="header").string
+
+        # print(className)
+
+        substitutions = classData.find_all("div", class_="row")
+        substitutionsArray = []
+        for substitution in substitutions:
+
+            hour = substitution.find("div", class_="period").string
+            # print(hour)
+        
+            info = substitution.find("div", class_="info").string 
+            # print(info)
+
+            substitutionsArray.append({"hour":hour, "info":info})
+           
+
+        if className: 
+          finalJson["substitutions"].append({
+            "className": className,
+            "substitutions":substitutionsArray
+        })
+
+
+    
+    return finalJson
+    
+print(get_substitutions("", "2024-03-15"))
